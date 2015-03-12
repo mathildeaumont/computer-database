9dc47cbaca9596d2775a5abe2e9f68aa546f5113
maumont <maumont@excilys.com>
2015-03-12 17:43:41 +0100

    CompanyModel

--- src/model/CompanyModel.java
+++ src/model/CompanyModel.java
@@ -2,4 +2,27 @@ package model;
 
 public class CompanyModel {
 
+	private long id;
+	private String name;
+	
+	public CompanyModel(long id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
