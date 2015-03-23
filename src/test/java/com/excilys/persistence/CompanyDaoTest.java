e4f2076c705b7928b9a4992dfb90a938a317357e
maumont <maumont@excilys.com>
2015-03-23 10:05:46 +0100

    Unit test

--- src/test/java/com/excilys/persistence/CompanyDaoTest.java
+++ src/test/java/com/excilys/persistence/CompanyDaoTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.After;
 import org.junit.Assert;
@@ -14,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.excilys.model.CompanyModel;
+import com.excilys.model.CompanyModelImpl;
 import com.excilys.util.DBUtil;
 
 
@@ -29,7 +31,7 @@ public class CompanyDaoTest {
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 		DBUtil.databaseTester.onTearDown();
@@ -40,11 +42,28 @@ public class CompanyDaoTest {
 		// GIVEN
 		DBUtil.cleanlyInsert(new FlatXmlDataSetBuilder().build(new File(
 				"src/test/java/com/excilys/persistence/data/getCompanies.xml")));
+		final int size = 2;
+		final CompanyModel company1 = new CompanyModelImpl(1, "Test");
+		final CompanyModel company2 = new CompanyModelImpl(2, "Test2");
 		// WHEN
-		 List<CompanyModel> companies = DaoFactory.INSTANCE.getCompanyDAO().getAllCompanies();
+		List<CompanyModel> companies = DaoFactory.INSTANCE.getCompanyDAO().getAllCompanies();
 		// THEN
-		Assert.assertNotNull(companies);
-		Assert.assertEquals(companies.size(), 2);
-		
+		Assertions.assertThat(companies).isNotNull();
+		Assertions.assertThat(companies).isNotEmpty();
+		Assertions.assertThat(companies.size()).isEqualTo(size);
+		Assertions.assertThat(companies).contains(company1);
+		Assertions.assertThat(companies).contains(company2);
+	}
+	
+	@Test
+	public void getAllCompaniesWhenEmptyDataset() throws Exception {
+		// GIVEN
+		DBUtil.cleanlyInsert(new FlatXmlDataSetBuilder().build(new File(
+				"src/test/java/com/excilys/persistence/data/getCompaniesEmpty.xml")));
+		// WHEN
+		List<CompanyModel> companies = DaoFactory.INSTANCE.getCompanyDAO().getAllCompanies();
+		// THEN
+		Assertions.assertThat(companies).isNotNull();
+		Assertions.assertThat(companies).isEmpty();
 	}
 }
