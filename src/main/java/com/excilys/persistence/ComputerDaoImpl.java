3ad1fa6d791628fc7584ed63145b0e6896aab33b
maumont <maumont@excilys.com>
2015-04-09 13:50:20 +0200

    fix(date)

--- src/main/java/com/excilys/persistence/ComputerDaoImpl.java
+++ src/main/java/com/excilys/persistence/ComputerDaoImpl.java
@@ -1,11 +1,6 @@
 package com.excilys.persistence;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +14,6 @@ import com.excilys.model.Page;
 
 @Repository
 public class ComputerDaoImpl implements ComputerDao {
-	
-	@Autowired
-	DaoFactory daoFactory;
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
