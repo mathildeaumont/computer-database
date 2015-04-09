e7cb6c41b66ffba94f24bb335c9d64da4aaebdd1
maumont <maumont@excilys.com>
2015-04-09 17:18:30 +0200

    fix(transactional dans service)

--- src/main/java/com/excilys/persistence/CompanyDaoImpl.java
+++ src/main/java/com/excilys/persistence/CompanyDaoImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.excilys.mapper.CompanyMapper;
 import com.excilys.model.CompanyModel;
